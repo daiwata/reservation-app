e5ded5dd1ddc092f03eba6f7d8c83f37d9e0219c
daiwata <watanabedai5427@gmail.com>
2020-03-17 00:43:03 +0900

    rename product-list to product-listing

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './product-listings/product-listings.component';
 import { Routes, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './product.component';
