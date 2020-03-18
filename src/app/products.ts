faf0a8b88c1f46b709c99b2249ee5afaaac8f8c1
daiwata <watanabedai5427@gmail.com>
2020-03-19 00:09:49 +0900

    heading, headingtext , listing-page product name, description

--- src/app/products.ts
+++ src/app/products.ts
@@ -2,21 +2,45 @@ export const products = [
   {
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    heading1: 'A large phone with one of the best screens1',
+    heading2: 'A large phone with one of the best screens2',
+    heading3: 'A large phone with one of the best screens3',
+    headingtext1: 'A large phone with one of the best screens1',
+    headingtext2: 'A large phone with one of the best screens2',
+    headingtext3: 'A large phone with one of the best screens3',
   },
   {
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    heading1: 'A large phone with one of the best screens1',
+    heading2: 'A large phone with one of the best screens2',
+    heading3: 'A large phone with one of the best screens3',
+    headingtext1: 'A large phone with one of the best screens1',
+    headingtext2: 'A large phone with one of the best screens2',
+    headingtext3: 'A large phone with one of the best screens3',
   },
   {
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    heading1: 'A large phone with one of the best screens1',
+    heading2: 'A large phone with one of the best screens2',
+    heading3: 'A large phone with one of the best screens3',
+    headingtext1: 'A large phone with one of the best screens1',
+    headingtext2: 'A large phone with one of the best screens2',
+    headingtext3: 'A large phone with one of the best screens3',
   },
   {
     name: 'Phone Special',
     price: 999,
-    description: ''
+    description: '',
+    heading1: 'A large phone with one of the best screens1',
+    heading2: 'A large phone with one of the best screens2',
+    heading3: 'A large phone with one of the best screens3',
+    headingtext1: 'A large phone with one of the best screens1',
+    headingtext2: 'A large phone with one of the best screens2',
+    headingtext3: 'A large phone with one of the best screens3',
   },
 ];
