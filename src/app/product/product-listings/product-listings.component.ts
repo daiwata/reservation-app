ad0d399a9ea84057d09948295cec63f75dc61092
daiwata <watanabedai5427@gmail.com>
2020-03-26 02:44:32 +0900

    observable sample1

--- src/app/product/product-listings/product-listings.component.ts
+++ src/app/product/product-listings/product-listings.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../shared/pruduct.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-listings',
@@ -14,6 +15,26 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
     this.products = this.productService.getProducts()
+
+    const observable = new Observable(subscriber => {
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next("ss");
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 3000);
+    });
+
+    debugger
+    console.log('subscribe前');
+    debugger
+    observable.subscribe({
+      next(x) { console.log('データ： ' + x); },
+      error(err) { console.error('エラー: ' + err); },
+      complete() { console.log('完了'); }
+    });
+    console.log('subscribeから抜けました！');
   }
 
 }
